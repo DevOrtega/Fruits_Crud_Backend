a22268f72a92daee181a0fd20bc0b7ea072e222d
Carlos Ortega <carloso2103@gmail.com>
2020-11-22 14:34:57 +0000

    Model, migration and seeder of Fruit

---
+++ database/seeds/FruitsSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class FruitsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('fruits')->insert([
+            'name' => 'Kiwi',
+            'size' => 'small',
+            'main_color' => 'green',
+        ]);
+        DB::table('fruits')->insert([
+            'name' => 'Banana',
+            'size' => 'medium',
+            'main_color' => 'yellow',
+        ]);
+        DB::table('fruits')->insert([
+            'name' => 'Watermelon',
+            'size' => 'big',
+        ]);
+    }
+}

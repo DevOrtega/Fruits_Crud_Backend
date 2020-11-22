a7ca61e4ae37d4350427baac12e1e50c2e6fc65f
Carlos Ortega <carloso2103@gmail.com>
2020-11-22 15:12:36 +0000

    Fix #1 Resources, controller and routes of Fruits completed

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/fruits', 'FruitController@index');
+Route::get('/fruits/{id}', 'FruitController@show');
+Route::post('/fruits', 'FruitController@store');
+Route::delete('/fruits/{id}', 'FruitController@delete');
+Route::patch('/fruits/{id}', 'FruitController@update');
